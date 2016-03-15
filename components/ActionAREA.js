4c9359ca64cab9a06814d5ee550c6a6449420ea6
Kaley <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-03-15 17:25:04 -0400

    button bindings work

--- components/ActionAREA.js
+++ components/ActionAREA.js
@@ -1,21 +1,25 @@
 var React = require('react');
+var $ = require('jQuery');
+var socket = io();
 
 //bind buttons----------------------------------------------------------------------------------------------------------
-/*$(document).ready(function(){
-  $("#endTurnButton").click(function(){
-    socket.emit("end turn");
-  });
 
-  $("#startGameButton").click(function(){
-    socket.emit("start game button");
-  });
-};
-*/
 
 //REACT CLASS
 
 var ActionAREA = React.createClass({
-	
+	componentDidMount: function(){
+		$("#endTurnButton").click(function(){
+			console.log("end turn click");
+	    	socket.emit("end turn");
+	  	});
+
+	  	$("#startGameButton").click(function(){
+	    	console.log("start game click");
+	    	socket.emit("start game button");
+	  	});
+	},
+
 
   render: function () {
   	var buttonStyle = {margin: 'auto', width: '100px', height: '50px'};
