aae39bfa665b718b61eb41be445b7a3ffe73bc58
Kaley <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-03-15 10:48:27 -0400

    no more separate dev-server file

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -25,5 +25,10 @@ module.exports = {
         include: path.join(__dirname, 'components')
       }
     ]
+  },
+  devServer: {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true
   }
 };
