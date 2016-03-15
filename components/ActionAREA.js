aefdb708d07a5322aa683a4b22d50e464b6501c0
Kaley <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-03-15 17:18:55 -0400

    buttons show up in action area

--- components/ActionAREA.js
+++ components/ActionAREA.js
@@ -1,11 +1,29 @@
 var React = require('react');
 
+//bind buttons----------------------------------------------------------------------------------------------------------
+/*$(document).ready(function(){
+  $("#endTurnButton").click(function(){
+    socket.emit("end turn");
+  });
+
+  $("#startGameButton").click(function(){
+    socket.emit("start game button");
+  });
+};
+*/
+
+//REACT CLASS
 
 var ActionAREA = React.createClass({
+	
+
   render: function () {
+  	var buttonStyle = {margin: 'auto', width: '100px', height: '50px'};
     return (
       <div className="layoutDIV" id='ActionAREA'>
         ActionAREA
+        <button id="startGameButton" style={buttonStyle} >Start Game</button>
+		<button id="endTurnButton" style={buttonStyle} >END TURN</button>
       </div>
     )
   }
