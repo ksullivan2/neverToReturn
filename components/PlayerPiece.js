bada156f5dd0e324c5574fb1e4379f5c6048ccd3
Kaley <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-03-18 15:22:13 -0400

    ok, so background opacity works... why doesn't image show up?

--- components/PlayerPiece.js
+++ components/PlayerPiece.js
@@ -10,12 +10,10 @@ var PlayerPiece = React.createClass({
     var self = this;
 
   	var border = '';
-  	var textColor = "black";
     var zIndex = "99";
 
     if (this.props.active){
     	border = "2px solid black";
-    	textColor = "white";
       zIndex = "100";
     }
 
@@ -24,17 +22,18 @@ var PlayerPiece = React.createClass({
               style={{left: spring(this.props.coords.left), bottom: spring(this.props.coords.bottom)}}>
         {function(interpolatingStyle){
           return(
-            <div className="playerPieceBox">
-              <div className="playerPiece" 
-                    style={{background: "radial-gradient("+self.props.player.color+" 0%, hsla(0, 100%, 20%, 0) 100%) 0 0",
-                            border: border,
-                            color: textColor,
-                            left: interpolatingStyle.left,
-                            bottom: interpolatingStyle.bottom,
-                            width: self.props.diameter,
-                            height: self.props.diameter,
-                            zIndex: zIndex}}>
-              	<p>{self.props.player.name}</p>
+            <div className="playerPiece" 
+                  style={{border: border,
+                        left: interpolatingStyle.left,
+                        bottom: interpolatingStyle.bottom,
+                        width: self.props.diameter,
+                        height: self.props.diameter,
+                        zIndex: zIndex}}>
+              <div className="playerThumbnail" style={{backgroundImage: "url('/assets/playerThumbnails/homelyVillager.jpg')"}} >
+                <div className="playerPieceColor" 
+                    /* style={{background: "radial-gradient("+self.props.player.color+" 0%, hsla(0, 100%, 20%, 0) 100%) 0 0"}}*/
+                    style= {{background: "rgba(10,100,20,.7)"}} >
+                </div>
               </div>
             </div>
           )
